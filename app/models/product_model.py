773351e033d6e39e32a4614d7eb2ade2144c4f96
leduy2004-coder <leduy8762@gmail.com>
2026-01-01 22:00:21 +0700

    feat: connect mysql

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -4,8 +4,9 @@ import datetime
 
 class Product(BaseModel):
     __tablename__ = "products"
+    
     id: int = Column(Integer, primary_key=True, index=True)
-    name: str = Column(String, index=True)
-    description: str = Column(String, index=True)
+    name: str = Column(String(255), index=True)
+    description: str = Column(String(1000), index=True)
     price: float = Column(Float, index=True)
     deleted_at: datetime.datetime = Column(DateTime, index=True, nullable=True)
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -4,10 +4,11 @@ from datetime import datetime
 
 class User(BaseModel):
     __tablename__ = "users"
+    
     id: int = Column(Integer, primary_key=True, index=True)
-    name: str = Column(String, index=True)
-    email: str = Column(String, index=True)
-    password: str = Column(String, index=True)
+    name: str = Column(String(100), index=True)
+    email: str = Column(String(255), index=True)
+    password: str = Column(String(255), index=True)
     status: int = Column(Integer, index=True, default=1)
     created_at: datetime = Column(DateTime, index=True, default=datetime.now)
     updated_at: datetime = Column(DateTime, index=True, default=datetime.now)
